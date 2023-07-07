d8444dccfc273eb9d50eecb7a4df7db2bc65e7e4
KeJunMao <kejun1997@gmail.com>
2023-07-07 14:49:57 +0800

    feat: add title for tag

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,7 @@ import { readFileSync } from "fs";
 import path from "path";
 import url from "url";
 import { defineConfig } from "vitepress";
+import grayMatter from "gray-matter";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -51,8 +52,46 @@ export default defineConfig({
       { icon: "github", link: "https://github.com/kejunmao/jsdoc" },
     ],
   },
-  srcExclude: [
-    'jsdoc.github.io'
-  ],
-  ignoreDeadLinks: true
+  srcExclude: ["jsdoc.github.io"],
+  ignoreDeadLinks: true,
+  markdown: {
+    config(md) {
+      md.use((md) => {
+        md.core.ruler.before("normalize", "tag-header", (state) => {
+          const { frontmatter = {} } = state.env;
+
+          if (
+            frontmatter.synonyms &&
+            frontmatter.synonyms.length
+          ) {
+            state.src = `## Synonyms\n${frontmatter.synonyms.map(
+              (v) => `- \`${v}\``
+            ).join('\n')}${state.src}`;
+          }
+
+          if (frontmatter.tag) {
+            state.src = `# @${frontmatter.tag}\n${frontmatter.description}\n${state.src}`;
+          }
+          if (frontmatter.inlineTag) {
+            state.src = `# \\{@${frontmatter.inlineTag}\\}\n${frontmatter.description}\n${state.src}`;
+          }
+        });
+
+        const render = md.render.bind(md);
+        md.render = (src, env = {}) => {
+          const { data } = grayMatter(src);
+          if (!data.title) {
+            if (data.tag || data.inlineTag) {
+              data.title = `@${data.tag || data.inlineTag}`;
+            }
+          }
+          env.frontmatter = {
+            ...env.frontmatter,
+            ...data,
+          };
+          return render(src, env);
+        };
+      });
+    },
+  },
 });
